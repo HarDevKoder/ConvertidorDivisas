c02b55d77b86800a9941efa998851bf72fbf0bc5
HarDevKoder <haroldvaldes@yahoo.com>
2024-01-07 03:05:04 -0500

    Creo Libreria de funciones, Resalto div seleccionado

---
+++ js/funciones.js
@@ -0,0 +1,21 @@
+// Referenciar los elementos del DOM
+export const referenciarElementosDom = () => {
+  return {
+    cop: document.querySelector("#cop"),
+    dolar: document.querySelector("#dolar"),
+    euro: document.querySelector("#euro"),
+  };
+};
+
+// Funcion que resalta el div seleccionado
+export const resaltarTarjetas = (divs) => {
+  divs.forEach(div=>{
+    div.addEventListener('click', ()=>{
+      // Desresalto todos los divs
+      divs.forEach(div => div.classList.remove("resaltado"));
+      // Resalto el div actual
+      div.classList.add("resaltado");
+    })
+  })
+}
+ 
---
+++ js/script.js
@@ -0,0 +1,18 @@
+// ----------------------------------------------------------------------------
+// IMPORTO FUNCIONES A UTILIZAR
+// ----------------------------------------------------------------------------
+import { referenciarElementosDom, resaltarTarjetas } from "./funciones.js";
+
+
+// ----------------------------------------------------------------------------
+// PROGRAMA PRINCIPAL
+// ----------------------------------------------------------------------------
+
+// referencio elementos del DOM
+referenciarElementosDom();
+
+// Creo Array con las tarjetas (divs)
+const divs = [cop, dolar, euro];
+
+// Resalto la tarjeta selecconada
+resaltarTarjetas(divs);
